5a7cce3720e792ada7aad52e29138a946f110b6e
Yosoi <yosoi@protonmail.com>
2020-06-24 00:03:01 -0400

    refactor log and translate methods

--- src/methods/internal/utils/log.js
+++ src/methods/internal/utils/log.js
@@ -1,5 +1,13 @@
 import validate from './validate.js'
 
 export default function (puzzle) {
-  
+  const validation = validate(puzzle);
+  if (!validation) throw validation;
+  console.log(puzzle.map((char, index) => {
+    let view = char + " ";
+    if (index % 3 === 2) view += "  ";
+    if (index % 9 === 8) view += "\n";
+    if (index % 27 === 26) view += "\n";
+    return view;
+  }).join());
 };
--- src/methods/internal/utils/validate.js
+++ src/methods/internal/utils/validate.js
@@ -1,3 +1,18 @@
-export default function (puzzle) {
-
+export default function (
+  puzzle,
+  blank = ".",
+  markers = "123456789",
+  size = 81
+) {
+  if (!puzzle) return "Empty board";
+  if (puzzle.length !== 81) {
+    return "Invalid board size. Board must be exactly " + size + " squares.";
+  }
+  const invalidChar = puzzle.find(char => {
+    return !(blank + markers).includes(char);
+  });
+  if (invalidChar) {
+    return "Invalid board character encountered: " + invalidChar;
+  }
+  return true;
 };
